67a4c473c84b59c863e50d904263dce54302c040
Hebert Silva <hebertpereirasilva@gmail.com>
2020-03-26 16:22:49 -0300

    fix: translatePath method request API

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,25 @@
 import client from './client'
 import { api } from './config'
 
+function translatePath (path) {
+  path = path.split('-')
+
+  if (path.length === 1) {
+    return path[0]
+  } else if (path.length > 1) {
+    const parts = path.slice(1).map((part) => {
+      return `${part[0].toUpperCase()}${part.slice(1)}`
+    })
+
+    return `${path[0]}${parts.join('')}`
+  } else {
+    return path
+  }
+}
+
 export default async (resource, method, payload = {}) => {
+  method = translatePath(method)
+
   const request = {
     payload,
     config: {}
